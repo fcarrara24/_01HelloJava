ab3d90586ff3e2b4a0dad4d4baf42e37fa02fd80
Francesco Carrara <f.carrara24@studenti.unibg.it>
2024-03-26 13:02:51 +0100

    added update

--- src/org/learning/basics/HelloJava.java
+++ src/org/learning/basics/HelloJava.java
@@ -1,5 +1,14 @@
 package org.learning.basics;
 
-public class HelloWorld {
-
+public class HelloJava {
+    /**
+     * per la creazione del main io posso crearlo con:   main + tab
+     * @param args
+     */
+    public static void main(String[] args) {
+        // per stampare il tutto posso fare sout
+        System.out.println("hello java!");
+        System.out.println(5+3);
+        System.out.println(5+3);
+    }
 }
